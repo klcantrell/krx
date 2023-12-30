3975ccf3712a30b0153540ff02582dbace91e288
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-12-29 23:21:07 -0500

    [TapToPayExperiment] Fixing Fastlane CI setup for iOS
    See if the order of the macOS security calls matters
    
    Potential fix for build freezing. See https://github.com/fastlane/fastlane/issues/15290.

--- TapToPayExperiment/MobileApp/ci/scripts/import-certificates.sh
+++ TapToPayExperiment/MobileApp/ci/scripts/import-certificates.sh
@@ -3,8 +3,10 @@
 set -euo pipefail
 
 security create-keychain -p "" build.keychain
-security set-keychain-settings # not sure what this does, but with additional parameters, can be used to set lock timeout. see https://github.com/Apple-Actions/import-codesign-certs/blob/5565bb656f60c98c8fc515f3444dd8db73545dc2/src/security.ts#L172-L178
+security list-keychains -s build.keychain # triggers an update of the keychain list. see https://github.com/Apple-Actions/import-codesign-certs/blob/5565bb656f60c98c8fc515f3444dd8db73545dc2/src/security.ts#L50-L64
+security default-keychain -s build.keychain # without this, build freezes waiting for a keychain password
 security unlock-keychain -p "" build.keychain
+security set-keychain-settings # not sure what this does, but with additional parameters, can be used to set lock timeout. see https://github.com/Apple-Actions/import-codesign-certs/blob/5565bb656f60c98c8fc515f3444dd8db73545dc2/src/security.ts#L172-L178
 
 echo $IOS_DIST_SIGNING_KEY | base64 --decode > signingCertificate.p12
 security import signingCertificate.p12 \
@@ -14,5 +16,3 @@ security import signingCertificate.p12 \
                 -T /usr/bin/codesign
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" build.keychain
-security list-keychains -s build.keychain # triggers an update of the keychain list. see https://github.com/Apple-Actions/import-codesign-certs/blob/5565bb656f60c98c8fc515f3444dd8db73545dc2/src/security.ts#L50-L64
-security default-keychain -s build.keychain # without this, build freezes waiting for a keychain password
